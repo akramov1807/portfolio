b3008bbaf92e2d3a7f0b42f0f5f1d201412bcfd4
akramov1807 <akramovh1807@gmail.com>
2024-06-30 00:25:29 +0500

    home finish

--- portfolio/src/pages/home/Home.js
+++ portfolio/src/pages/home/Home.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import './home.css'
+import Profile from '../../images/photo.jpg'
+import { Link } from 'react-router-dom'
+import { FaArrowRight } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className='home section grid'>
+      <img className='home__img' src={Profile} alt='...'/>
+
+      <div className='home__content'>
+        <div className='home__data'>
+          <h1 className='home__title'>
+            <span>I'm Husniddin Akramov.</span> Web Developer
+          </h1>
+          <p className='hame_description'>
+            I'm a Ferghana web developer & passionate about creating great software that improves the lives of those around me.
+          </p>
+          <Link to={'/about'} className='button'>
+            More About Me{''}
+            <span className='button__icon'>
+              <FaArrowRight/>
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className='color__block'></div>
+    </section>
   )
 }
